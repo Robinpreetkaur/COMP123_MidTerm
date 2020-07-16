63c141eb10034cd678fc53a60096e3ba1d858bd6
Robinpreet <rkaur750@my.centennialcollege.ca>
2020-07-15 21:13:38 -0400

    Add project files.

---
+++ COMP123_MidTerm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP123_MidTerm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
